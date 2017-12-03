247e16995883556faa88fcce5f9ea13c87cb230a
KalirajGit <kalirajfss@gmail.com>
2017-12-03 17:04:33 +0530

    Java file modified

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
    for(int i=0;i<10;i++){
 		System.out.println("Hello, World :"+i);
 	  }
-	  
+	System.out.println("Welcome to Hello World program");  
    }
-   System.out.println("Welcome to Hello World program");
+   
 }
